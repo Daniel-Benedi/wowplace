88850f9f0de78b0d390228874866c113060cb8f7
Daniel Benedi <daniel@benedi.app>
2025-02-15 11:53:59 +0000

    Setup Vuetify and frontend skeleton

---
+++ nuxt.config.ts
@@ -0,0 +1,31 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
+export default defineNuxtConfig({
+  build: {
+    transpile: ['vuetify']
+  },
+  modules: [(_options, nuxt) => {
+    nuxt.hooks.hook('vite:extendConfig', (config) => {
+      // @ts-expect-error
+      config.plugins.push(vuetify({ autoImport: true }))
+    })
+  }],
+  compatibilityDate: '2024-11-01',
+  devtools: { enabled: true },
+  vite: {
+    vue: {
+      template: {
+        transformAssetUrls,
+      }
+    }
+  },
+  routeRules: {
+    '/login': { ssr: false },
+    '/signup': { ssr: false },
+    '/order/**': { ssr: false },
+    '/account/**': { ssr: false },
+    '/listing/**': { ssr: true },
+    '/profile/**': { ssr: true },
+    '/api/**': { cors: true }
+  }
+})
